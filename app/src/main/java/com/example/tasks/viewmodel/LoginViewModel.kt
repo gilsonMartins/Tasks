00d1918bf5dfb763d7e4b8d722542767662ebc73
gilson.martins <gilson.martins@itbam.org.br>
2021-06-23 20:58:52 -0400

    :hammer: config Callbacks
    validate callback

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,16 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val mPersonREpository = PersonRepository()
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
+        mPersonREpository.login(email, password)
     }
 
     /**
